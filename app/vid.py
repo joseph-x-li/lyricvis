5c15eff1b3a7fd2a2909247e88c627d737746637
Joseph Li <13826851+joseph-x-li@users.noreply.github.com>
2022-10-02 05:23:01 -0400

    asdf

--- app/vid.py
+++ app/vid.py
@@ -5,6 +5,10 @@ from moviepy.video.compositing.CompositeVideoClip import CompositeVideoClip
 from PIL import Image
 
 def create_video(subtitles, image_paths):
+  if subtitles[0][0] > 0: # start of first subtitle is not @ 00:00
+    # 0 -> begin, same path
+    # add a fake frame
+    images_paths = [(0.0, image_paths[0][0], image_paths[0][1])] + images_paths
   image_clips = [ImageClip(x) for x in image_paths]
   image_clips_dur = []
   for (start, end, _), ic in zip(subtitles, image_clips):
@@ -13,7 +17,7 @@ def create_video(subtitles, image_paths):
     image_clips_dur.append(ic.set_duration(f"00:0{mins}:{secs:.2f}"))
   vid = concatenate_videoclips(image_clips_dur)
 
-  gen = lambda txt: TextClip(txt, font='Georgia-Regular', fontsize=24, color='white')
+  gen = lambda txt: TextClip(txt, font='Georgia-Regular', fontsize=18, color='white', method="caption", size=512, align="South")
   sub = SubtitlesClip("song.mp4.en.srt", gen)
 
   vid = CompositeVideoClip([vid, sub])
